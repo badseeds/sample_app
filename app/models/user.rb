751f3d74771a92537312ccec00682ad77595ec03
Panikos Stavrou <omnis.soft@gmail.com>
2014-10-21 18:07:51 +0300

    Added a stub User show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,12 @@
 #
 
 class User < ActiveRecord::Base
-	
+
   validates :name, :presence    => true, 
                    :length      => {:maximum => 50 }
 
   Valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   validates :email, :presence   => true, 
                     :format     => { :with => Valid_email_regex },
                     :uniqueness => {:case_sensitive => false}
